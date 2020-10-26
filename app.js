179c4e7a0f49b99293d9f3b035787534d0d16591
maddclif <maddclif24@gmail.com>
2020-10-27 02:39:03 +0300

    add routing

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/bomberman', (req, res) => {
+    app.use(express.static('public'));
+    res.send('./public/index.html');
+});
+
+app.listen(8083, () => {
+    console.log('Example app listening on port 8083!');
+});
